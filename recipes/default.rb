ab915b77685ad3f08aaf19222ea2c5e8af0acbdf
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-01-11 17:11:00 +0800

    Add ssl import

--- recipes/default.rb
+++ recipes/default.rb
@@ -64,70 +64,26 @@ cookbook_file '/var/lib/rabbitmq/.erlang.cookie' do
   notifies :restart, "service[rabbitmq-server]"
 end
 
-
-#Import ssl for Nginx to truststore. 
-#include_recipe "propel_nginx::import_ssl"
-
 #Import ssl cert for rabbitmq
-#cookbook_file '/tmp/ssl_crt_rabbitmq.tar.gz' do
-#  source 'ssl_crt_rabbitmq.tar.gz'
-#  mode '0755'
-#end
+cookbook_file '/tmp/ssl_crt_rabbitmq.tar.gz' do
+  source 'ssl_crt_rabbitmq.tar.gz'
+  mode '0755'
+  notifies :run, "bash[import-ssl]"
+end
 
-#if ( node.ipaddress =~ /30.161(.*)/ )
-#if ( node.ipaddress =~ /15.107(.*)/ )
-
-#  puts "This is the N1 environment"
-#  bash "import-ssl" do 
-#   cwd   '/tmp'
-#  code <<-EOH
-#      tar zxf /tmp/ssl_crt_rabbitmq.tar.gz 
-#      if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2014" | grep -q propel-ha-5
-#        then
-#      keytool -importcert -file /tmp/ssl_crt/propel-ha-5.hp.com_propel_host.crt -keystore /opt/hp/propel/security/propel.truststore -alias "propel-ha-5.hp.com" -storepass "propel2014" -noprompt
-#      fi
-#      if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2014" | grep -q propel-ha-6
-#        then
-#      keytool -importcert -file /tmp/ssl_crt/propel-ha-6.hp.com_propel_host.crt -keystore /opt/hp/propel/security/propel.truststore -alias "propel-ha-6.hp.com" -storepass "propel2014" -noprompt
-#      fi
-#      EOH
-#  end
-#else
-#  puts "This is the Prod environment"
-#  bash "import-ssl" do 
-#   cwd   '/tmp'
-#   code <<-EOH
-#      tar zxf /tmp/ssl_crt_rabbitmq.tar.gz 
-#    if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2014" | grep -q atc-cr-wls3
-#        then
-#      keytool -importcert -file /tmp/ssl_crt/atc-cr-wls3_propel_host.crt -keystore /opt/hp/propel/security/propel.truststore -alias "atc-cr-wls3" -storepass "propel2014" -noprompt
-#      fi
-#      if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2014" | grep -q atc-cr-wls4
-#        then
-#      keytool -importcert -file /tmp/ssl_crt/atc-cr-wls4_propel_host.crt -keystore /opt/hp/propel/security/propel.truststore -alias "atc-cr-wls4" -storepass "propel2014" -noprompt
-#      fi
-#      EOH
-#  end
-#end
-
-#Configure nginx configuration file for reserve proxy
-#if server_ipaddress == node[:propel_rabbitmq][:propel_backend_2]
-#  template "/tmp/add_cluster.sh" do
-#    source "add_cluster.sh"
-#    mode '0755'
-#    variables ({
-#      :clustermaster => node[:propel_rabbitmq][:propel_backend_1_name],
-#    })
-#  end
-#enable rabbitmq management plugin in node1
-#  execute "add_cluster" do
-#    user "root"
-#    command "/tmp/add_cluster.sh"
-#  end
-#end
-
-#enable rabbitmq management plugin in node1
-#execute "enable rabbitmq management plugin in node1" do
-#  user "root"
-#  command "rabbitmq-plugins enable rabbitmq_management"
-#  end
+  bash "import-ssl" do 
+   cwd   '/tmp'
+   action :nothing
+   code <<-EOH
+          dir_name="/tmp/ssl_crt/"
+          all=`ls  ${dir_name}`
+          for i in $all
+            do
+            nodename=${i%_pro*}
+            if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2015" | grep -q $nodename}
+                      then
+                        keytool -importcert -file /tmp/ssl_crt/"$nodename"_propel_host.crt -keystore /opt/hp/propel/security/propel.truststore -alias "$nodename" -storepass "propel2016" -noprompt
+            fi
+            done
+      EOH
+  end
--- recipes/ssl_import.rb
+++ recipes/ssl_import.rb
@@ -1,27 +1,24 @@
-directory '/tmp/propel' do
+
+#Import ssl cert for rabbitmq
+cookbook_file '/tmp/ssl_crt.tar.gz' do
+  source 'ssl_crt.tar.gz'
   mode '0755'
-  action :create
-end 
-
-search(:node, 'name:*') do |n|
-    if n["ssl_cert"]
-        host_name = n["fqdn"]
-        f = File.open("/tmp/propel/#{host_name}.crt", 'w')
-        f.puts n["ssl_cert"]
-        f.close
-    end
+  notifies :run, "bash[import-ssl]"
 end
 
-bash "import-ssl" do 
- cwd   '/tmp/propel'
- code <<-EOH
-  for file in ./*
-    file_name=${file%.crt}
-    do
-      if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2014" | grep -q $file_name
-      then
-        keytool -importcert -file $file -keystore /opt/hp/propel/security/propel.truststore -alias "$file_name" -storepass "propel2014" -noprompt
-      fi
-    done
-  EOH
-end
+  bash "import-ssl" do 
+   cwd   '/tmp'
+   action :nothing
+   code <<-EOH
+          dir_name="/tmp/ssl_crt/"
+          all=`ls  ${dir_name}`
+          for i in $all
+            do
+            nodename=${i%_pro*}
+            if ! keytool -list -keystore /opt/hp/propel/security/propel.truststore -storepass "propel2015" | grep -q $nodename}
+                      then
+                        keytool -importcert -file /tmp/ssl_crt/"$nodename"_propel_host.crt -keystore /opt/hp/propel/security/propel.truststore -alias "$nodename" -storepass "propel2016" -noprompt
+            fi
+            done
+      EOH
+  end
