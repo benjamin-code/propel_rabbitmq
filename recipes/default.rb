2782af50929ea7a26fe74752c95517d64dcd5a22
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-01-14 18:13:14 +0800

    Correct service restart error

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,15 +29,15 @@ if ( node.hostname =~ /propel-ha(.*)/ )
     remote_file "/tmp/rabbitmq-server-3.5.4-1.noarch.rpm" do
       source 'http://30.161.224.150/rpm/rabbitmq-server-3.5.4-1.noarch.rpm' 
       mode "0755"
-      notifies   :run, 'execute[Install-rabbitmq-server]', :immediately
-      notifies :run, "bash[env-check]", :immediately
-      notifies :run, "bash[Add-user]", :immediately
       action  :create_if_missing
     end
-    execute "Install-rabbitmq-server" do
-      user "root"
-      command "rpm -Uvh /tmp/rabbitmq-server-3.5.4-1.noarch.rpm"
-      action :nothing
+   
+  bash "Install-rabbitmq-server" do
+    code <<-EOH
+         if [ ! -f "/etc/init.d/rabbitmq-server" ]; then
+            rpm -Uvh /tmp/rabbitmq-server-3.5.4-1.noarch.rpm
+         fi
+        EOH
     end
 end
 
@@ -45,33 +45,12 @@ if ( node.hostname =~ /atc(.*)/ )
   yum_package 'rabbitmq-server' do
     action :install
     flush_cache [ :before ]
-    notifies :run, "bash[Add-user]"
   end
 end
 
 service 'rabbitmq-server' do
     service_name 'rabbitmq-server'
   action [:enable, :start]
-end
-
-bash "env-check" do
-    action  :nothing
-    code <<-EOH
-        pid=`pidof beam.smp`
-            if [ $pid -ne 0 ];then
-               kill -9 $pid
-               service rabbitmq-server restart
-            fi
-    EOH
-end
-
-bash "Add-user" do
-    action  :nothing
-    code <<-EOH
-      rabbitmqctl add_user rabbit_sx propel2014
-      rabbitmqctl set_user_tags rabbit_sx administrator
-      rabbitmqctl set_permissions -p / rabbit_sx ".*" ".*" ".*"
-    EOH
 end
 
 cookbook_file '/var/lib/rabbitmq/.erlang.cookie' do
