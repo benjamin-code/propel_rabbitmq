794ec3b9cb1354f5f46c1e3f4203326e35ee780c
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-01-25 15:00:19 +0800

    Add FT1 support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,17 +1,17 @@
-#Prod
+if ( node.hostname =~ /atc(.*)/ )
+  puts "This is the Prod environment."
 default[:propel_rabbitmq][:propel_backend_1] = "atc-cr-wls3"
 default[:propel_rabbitmq][:propel_backend_2] = "atc-cr-wls4"
-
-#Test
-if ( node.hostname =~ /centos6(.*)/ )
-  puts "This is the test environment"
-default[:propel_rabbitmq][:propel_backend_1] = "centos6-5"
-default[:propel_rabbitmq][:propel_backend_2] = "centos6-6"
 end
 
-#N1
 if ( node.hostname =~ /propel-ha(.*)/ )
   puts "This is the N1 environment"
 default[:propel_rabbitmq][:propel_backend_1] = "propel-ha-5"
 default[:propel_rabbitmq][:propel_backend_2] = "propel-ha-6"
+end
+
+if ( node.hostname =~ /pln(.*)/ )
+  puts "This is the FT1 environment"
+default[:propel_rabbitmq][:propel_backend_1] = "pln-cd1-iweb3"
+default[:propel_rabbitmq][:propel_backend_2] = "pln-cd1-iweb4"
 end
