f9bb0f88678ba5a19c3bc375a5386188a7b81c02
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-01-28 14:09:37 +0800

    Select environment based on chef_environment

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,13 +4,13 @@ default[:propel_rabbitmq][:propel_backend_1] = "atc-cr-wls3"
 default[:propel_rabbitmq][:propel_backend_2] = "atc-cr-wls4"
 end
 
-if ( node.hostname =~ /propel-ha(.*)/ )
+if node.chef_environment == 'sandbox'
   puts "This is the N1 environment"
 default[:propel_rabbitmq][:propel_backend_1] = "propel-ha-5"
 default[:propel_rabbitmq][:propel_backend_2] = "propel-ha-6"
 end
 
-if ( node.hostname =~ /pln(.*)/ )
+if node.chef_environment == 'env1'
   puts "This is the FT1 environment"
 default[:propel_rabbitmq][:propel_backend_1] = "pln-cd1-iweb3"
 default[:propel_rabbitmq][:propel_backend_2] = "pln-cd1-iweb4"
