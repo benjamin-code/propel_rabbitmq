6bf673ae44c400c56957772fc92f6d876c263eb1
Wang Yun <yun.wang2@hpe.com>
2016-03-15 14:04:25 +0800

    install rabbitmq3.6.1
    Change-Id: I54d94b9fc9c0f61ebd566adace5d415ffb9d06db

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,28 +17,50 @@ yum_package 'erlang' do
   flush_cache [ :before ]
 end
 
-if ( node.hostname =~ /propel-ha(.*)/ )
-    remote_file "/tmp/rabbitmq-server-3.5.4-1.noarch.rpm" do
-      source 'http://30.161.224.150/rpm/rabbitmq-server-3.5.4-1.noarch.rpm' 
-      mode "0755"
-      action  :create_if_missing
-    end
-   
-  bash "Install-rabbitmq-server" do
-    code <<-EOH
-         if [ ! -f "/etc/init.d/rabbitmq-server" ]; then
-            rpm -Uvh /tmp/rabbitmq-server-3.5.4-1.noarch.rpm
-         fi
-        EOH
-    end
+rpm_dir = "#{node['propel_ha']['git_dir']}"
+directory rpm_dir do
+  owner 'root'
+  group 'root'
+  mode  "0755"
+  recursive true
 end
 
-if node.chef_environment == 'env1' || node.chef_environment == 'prod'
-  yum_package 'rabbitmq-server' do
-    action :install
-    flush_cache [ :before ]
-  end
+rabbitmq_rpm = rpm_dir + '/rabbitmq-server-3.6.1-1.noarch.rpm'
+cookbook_file rabbitmq_rpm do
+  source 'rabbitmq-server-3.6.1-1.noarch.rpm'
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
+
+execute "install_rabbitmq" do
+	cwd rpm_dir
+	command "rpm -Uvh rabbitmq-server-3.6.1-1.noarch.rpm"
+  not_if  "[ -f /etc/init.d/rabbitmq-server ]"
+end
+
+#if ( node.hostname =~ /propel-ha(.*)/ )
+#  remote_file "/tmp/rabbitmq-server-3.5.4-1.noarch.rpm" do
+#    source 'http://30.161.224.150/rpm/rabbitmq-server-3.5.4-1.noarch.rpm' 
+#    mode "0755"
+#    action  :create_if_missing
+#  end
+#   
+#  bash "Install-rabbitmq-server" do
+#    code <<-EOH
+#         if [ ! -f "/etc/init.d/rabbitmq-server" ]; then
+#            rpm -Uvh /tmp/rabbitmq-server-3.5.4-1.noarch.rpm
+#         fi
+#        EOH
+#  end
+#end
+
+#if node.chef_environment == 'env1' || node.chef_environment == 'prod'
+#  yum_package 'rabbitmq-server' do
+#    action :install
+#    flush_cache [ :before ]
+#  end
+#end
 
 service 'rabbitmq-server' do
     service_name 'rabbitmq-server'
